d3b31c93c2be644402e4a122e42cec8ecee293c4
Prateeksingchn <prateeksinghchouhan4499@gmail.com>
2024-07-17 18:38:57 +0530

    sidenav added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import Sidenav from '../partials/Sidenav';
+
 
 const Home = () => {
+  document.title = "Movie App | Homepage";
   return (
-    document.title = "Movie App | Homepage",
-    <div>Home</div>
-  )
+   <>
+      <Sidenav />
+      <div className='w-[80%] h-full '></div>
+   </>
+  );
 }
 
 export default Home
